ca8cd890a2d5ec1b88550e2a8d9556ee64955dfe
leminlimez <jirizarry317@gmail.com>
2023-08-01 12:29:30 -0400

    Initial Commit

---
+++ Chicken Butt/Chicken_ButtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chicken_ButtApp.swift
+//  Chicken Butt
+//
+//  Created by lemin on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Chicken_ButtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chicken Butt/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Chicken Butt
+//
+//  Created by lemin on 8/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
